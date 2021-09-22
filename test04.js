6faeb42079030e72763d1455a5a835a7db324fda
nuuco <nuuco.coding@gmail.com>
2021-09-23 06:08:17 +0900

    test04.js 수정

--- test04.js
+++ test04.js
@@ -58,10 +58,116 @@ var 변수를 알긴 하지만 undefined 값이기 때문에 함수가 아니하
 */
 
 
-hello8();
+// hello8();
 
-const hello8 = function() {
-    console.log('hello8');
-}
+// const hello8 = function() {
+//     console.log('hello8');
+// }
 //결과 :
 //ReferenceError: Cannot access 'hello8' before initialization
+
+//생성자 함수
+const sum = new Function('a', 'b', 'c', 'return a + b + c');
+
+console.log(sum(1, 2, 3));
+
+//매개변수 a, b, c를 받아서 마지막 코드내용을 실행
+//호출 먼저하면 정의되지 않았다고 뜬다.
+//ReferenceError: Cannot access 'sum' before initialization
+
+console.log('< function 과 new Function() 차이점 >');
+
+
+global.a = 0;
+{
+    const a = 1;
+    const test = new Function('return a');
+
+    console.log(test());
+}
+//결과 : 0
+//const a 에는 접근 못함
+//new Function 상위 범위의 변수에 접근할 수 없다.
+//단, global 변수에는 접근 가능.
+
+{
+    const a = 1;
+    const test = function() {
+        return a;
+    }
+    console.log(test());
+}
+//결과 : 1
+//global.a 가 아닌 스코프 안의 상위 const a 에 접근한다.
+
+console.log('< arrow function >');
+
+const hello9 = () => {
+    console.log('hello9');
+}
+//선언적 방식으로 쓸 수는 없다. 항상 익명 함수
+
+//매개 변수가 하나일 때, 괄호 생략 가능.
+//매개 변수가 하나가 아닐 때는 항상 괄호 필요!
+const hello10 = text => {
+    console.log('hello10', text);
+} 
+
+//함수 리턴
+//리턴 값만 있을 경우 중괄호{}를 생략하고 쓸 수 있다.
+// const hello11 = age => {
+//     return `hello11 ${age}`;
+// };
+
+//위와 동일
+const hello11 = age => `hello11 ${age}`;
+
+console.log(hello11(22));
+
+console.log('< 생성자 함수 >');
+//생성자 함수를 이용하여 새로운 객체를 만들어 내는 방법
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+    //인자로 넣어준 값을 객체가 프로퍼티로 가지고 있게 만드는 것.
+}
+
+const p = new Person('Dave', 37);
+console.log(p, p.name, p.age);
+//결과 :
+//Person { name: 'Dave', age: 37 } Dave 37
+
+const p2 = new Person('Anna', 25);
+//여러 객체를 만들 수 있다.
+
+//함수는 객체이기 때문에, 함수 안에서 함수를 만들어 리턴 가능하다.
+function plus(base) {
+    return function(num) {
+        return base + num;
+    }
+}
+
+const plus5 = plus(5);  //plus5 = num => 5 + num;
+console.log(plus5(10)); //결과 : 15
+
+const plus7 = plus(7);  //plus7 = num => 7 + num;
+console.log(plus7(10)); //결과 : 17
+
+
+console.log('< 인자로 함수를 가진 함수, 콜백 함수 >');
+//함수를 호출할 때, 인자로 함수를 사용 가능
+
+function hello(callback){
+    console.log('hello');
+    callback()
+;}
+
+hello(function(){
+    console.log('콜백');
+})
+//결과 :
+/*
+hello
+콜백
+*/
