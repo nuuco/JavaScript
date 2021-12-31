25f68048fa8fcc7c6e9395a1319cbe0655646eb9
nuuco <nuuco.coding@gmail.com>
2021-12-31 23:30:34 +0900

    문자열 추가 수정

--- fc_JSmaster/2_05_문자열.js
+++ fc_JSmaster/2_05_문자열.js
@@ -23,3 +23,41 @@ console.log(multiLine);
 const empty = null;
 console.log(empty + 'hi');  //'nullhi'
 empty = '';  //빈 따옴표로 충분하다.
+
+//표준내장객체
+//String MDN 검색해서 표준내장객체 확인 가능
+const str = "ABC";
+str.toLowerCase();  //abc
+//문자열 인덱스 - 0, 1, 2 ... 배열처럼 취급
+str.charAt(2);  //C
+str[1]; //B
+//특정 문자 찾기
+const paragraph = "The quick brown fox jumps over the lazy dog.";
+const searchTerm = "dog";
+//includes()-> 포함여부
+paragraph.includes(searchTerm); //true
+paragraph.includes('hi');   //false
+//indexOf()-> 포함여부 + 인덱스 번호(없으면 -1)
+paragraph.indexOf(searchTerm);  //40
+paragraph.indexOf('hi');    //-1
+
+
+//객체(배열)를 문자열로 바꾸면
+String({name: 'john'}); //[object Object]
+String([1, 2, 3]); //1, 2, 3[object Object]
+//-> 이렇게 나오기 때문에 JSON 사용해야함
+JSON.stringify({name: 'john'}); //{"name": "john"}
+JSON.stringify([1, 2, 3]); //[1, 2, 3]
+
+//toString() - Number, Object, Date 등 다양한 객체에 들어있는
+//문자열로 변환시키는 메서드
+['1', 123].toString();  //1,123
+
+//그 외 상황에서는 문자열 변환은 String()만으로 충분!
+
+
+
+
+
+
+
