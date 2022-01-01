3023d6ab155e19b63e70de7256ff8b9d9087a411
nuuco <nuuco.coding@gmail.com>
2022-01-01 23:30:09 +0900

    문자열 추가 수정

--- fc_JSmaster/2_05_문자열.js
+++ fc_JSmaster/2_05_문자열.js
@@ -12,15 +12,15 @@ const multiLine =
 3'; //이렇게 \를 써줘야했다. 안 쓰면 오류남.
 
 ///하지만 ES2015+ 부터 백킷을 쓸 수 있어 개행 넣기 편해짐
-multiLine =
+const multiLine2 =
 `1
 2
 3`;
 
-console.log(multiLine);
+console.log(multiLine2);
 
 //빈문자열 만들 때 null 넣지말것. 연산할 때 문자열로 인식
-const empty = null;
+let empty = null;
 console.log(empty + 'hi');  //'nullhi'
 empty = '';  //빈 따옴표로 충분하다.
 
@@ -56,8 +56,49 @@ JSON.stringify([1, 2, 3]); //[1, 2, 3]
 //그 외 상황에서는 문자열 변환은 String()만으로 충분!
 
 
+//문자열 병합
+function genHello(name){
+    const resultName = name || '이름없음'; //기본값 넣기
+    return '안녕하세요. ' + resultName + '님 반갑습니다.';
+}
+
+genHello('nuuco');
+
+
+//문자열과 배열
+//문자열 -> 배열 = .split('');  괄호 안에 나누는 기준이 되는 문자열 넣어준다.
+console.log('HELLO,WORLD'.split(''));   //['H', 'E', 'L', 'L', 'O', ',', 'W', 'O', 'R', 'L', 'D']
+console.log('HELLO,WORLD'.split(','));  //[ 'HELLO', 'WORLD' ]
+//또는 전개 연산자를 이용해 문자열 -> 배열 전환 가능
+const helloWorld = 'HELLO WORLD';
+console.log([...helloWorld]);   //['H', 'E', 'L', 'L', 'O', ',', 'W', 'O', 'R', 'L', 'D']
+
+
+//배열 -> 문자열 = .join('');
+console.log(['H', 'E', 'L', 'L', 'O', ',', 'W', 'O', 'R', 'L', 'D'].join(''));  //HELLO,WORLD
+console.log(['H', 'E', 'L', 'L', 'O', ',', 'W', 'O', 'R', 'L', 'D'].join('-'));  //H-E-L-L-O-,-W-O-R-L-D
+
+
+//배열과 문자열 모두 length 속성을 사용할 수 있다.
+console.log('hello'.length); //5
 
 
 
+/**ES2015+
+ * Template Literals
+ * 1. 멀티라인 (개행)이 자유로움
+ * 2. Basic String Formatting => 보간법 활용 => 표현식을 문자열에 포함
+ * 3. HTML Escaping: 안전하게 사용하도록 도움 (XSS, SQL, Injection)
+ * 
+ */
+//보안에 취약한 예시
+const htmlTemplate = '<div>안녕하세요</div>';
+//body.innerHTML = htmlTemplate;
 
+//보안이 좀 더 강화된 예시
+function genHi(name){
+    return `<div>안녕하세요. ${name}님 환영합니다.</div>`;
+}
+const divTag = genHi('nuuco');
+//body.innerHTML = divTag;
 
